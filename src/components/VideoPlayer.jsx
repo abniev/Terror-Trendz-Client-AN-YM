a5c5db84919f3c93f6cd6741defd261599b42f9f
Abner Nieves <nieves.abner@gmail.com>
2024-06-27 16:28:26 -0400

    updating

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -8,8 +8,8 @@ function VideoPlayer() {
   const [videoList, setVideoList] = useState("");
   const { deleteMusic } = useContext(AuthContext);
   const { user } = useContext(AuthContext);
-
   const { music } = useContext(MusicContext);
+
   useEffect(() => {
     music && setVideoList(music[Math.floor(Math.random() * music.length)]);
   }, [music]);
@@ -61,16 +61,16 @@ function VideoPlayer() {
                 </svg>
               </button>
             </div>
-            <div>
-              {user && user.isAdmin && (
+            {user && user.isAdmin && (
+              <div>
                 <button
                   className="mt-2 px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-yellow-400 focus:ring-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-yellow-400"
                   onClick={() => deleteMusic(music._id)}
                 >
                   Delete Video
                 </button>
-              )}
-            </div>
+              </div>
+            )}
             <p className="mt-5 border-b-2 border-gray-900"></p>
           </div>
         ))
