5c11d679bfda129065414d1afbc55106895d2238
Abner Nieves <nieves.abner@gmail.com>
2024-06-27 16:39:56 -0400

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -121,32 +121,30 @@ function Navbar() {
                     />
                   </svg>
                 </Link>
+                <Link
+                  type="button"
+                  className="m-8 block flex flex-wrap font-sophisto flex gap-2 p-4 text-black border border-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                  to="/login"
+                >
+                  Log in
+                  <svg
+                    className="rtl:rotate-180 w-3.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </Link>
               </div>
             )}
-            <div>
-              <Link
-                type="button"
-                className="m-8 block flex flex-wrap font-sophisto flex gap-2 p-4 text-black border border-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                to="/login"
-              >
-                Log in
-                <svg
-                  className="rtl:rotate-180 w-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </Link>
-            </div>
           </div>
         </div>
       </div>
