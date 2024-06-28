37f0aedf25de68a62a3e20114c59ca4eae742ae2
Abner Nieves <nieves.abner@gmail.com>
2024-06-28 12:06:41 -0400

    hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,6 +43,7 @@ function Navbar() {
             <div className="flex flex-wrap">
               <Link
                 className="block m-5 p-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-500 md:p-5 dark:text-white md:dark:hover:text-green-500 dark:hover:bg-green-500 dark:hover:text-white md:dark:hover:bg-transparent"
+                aria-current="page"
                 to="/product"
               >
                 Products
